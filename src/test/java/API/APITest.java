6e479774eba6489e42416c375a955c94b24c1bfb
emailtovijay-tech <emailtovijay050390@gmail.com>
2025-10-14 12:20:39 +0530

    This is 1st commit

---
+++ src/test/java/API/APITest.java
@@ -0,0 +1,23 @@
+package API;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class APITest {
+
+    @Test
+    public void APIDemo()
+
+    {
+        Response response = RestAssured.get("https://fakestoreapi.com/products");
+
+        System.out.println("Status Code: " +response.getStatusCode());
+        System.out.println("Response Body: " + response.getBody().asString());
+        System.out.println("Response Time: " +response.getTime() + " ms");
+
+        int status_code = response.getStatusCode();
+        Assert.assertEquals(status_code, 200);
+    }
+}
